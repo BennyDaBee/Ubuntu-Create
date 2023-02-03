c55c9690e7ec10bee1458b6bf3b03b8bcf095d5b
BennyDaBee <97147515+BennyDaBee@users.noreply.github.com>
2023-02-03 12:53:11 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+BLUE='\033[0;34m'
+WHITE='\033[0;37m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+if systemctl --all --type service | grep -q 'ajenti'; then
+echo -e "${BLUE}Ajenti is Installed"
+else
+echo -e "${RED}Installing Ajenti"
+curl https://raw.githubusercontent.com/ajenti/ajenti/master/scripts/install.sh | sudo bash -s -
+fi
+
+if systemctl --all --type service | grep -q 'docker'; then
+echo -e "${BLUE}Docker is Installed"
+else
+echo -e "${RED}Installing Docker"
+sudo apt-get -y install docker.io
+sudo snap install docker
+sudo groupadd docker
+sudo usermod -aG docker bennythebee
+newgrp docker
+fi
+
+
+echo -e "${NC}Script Complete"
