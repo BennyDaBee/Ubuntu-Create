cfb9f7cd768c38550cf11da0b1b7dff058ea2ada
BennyDaBee <97147515+BennyDaBee@users.noreply.github.com>
2023-08-03 17:40:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,15 @@ WHITE='\033[0;37m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-echo -e "{RED} Installing Updates"
+echo -e "${BLUE}Disabling IPv6"
+touch /etc/sysctl.d/60-custom.conf
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.d/60-custom.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.d/60-custom.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.d/60-custom.conf
+sudo sysctl -p
+sudo systemctl restart procps
+
+echo -e "${RED} Installing Updates"
 sudo apt-get upgrade -y
 
 if systemctl --all --type service | grep -q 'docker'; then
@@ -25,13 +33,5 @@ echo -e "${RED}Configuring SSH"
 echo 'HostKeyAlgorithms +ssh-rsa' >> /etc/ssh/sshd_config
 sudo systemctl restart ssh
 fi
-
-echo -e "${BLUE}Disabling IPv6"
-touch /etc/sysctl.d/60-custom.conf
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.d/60-custom.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.d/60-custom.conf
-echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.d/60-custom.conf
-sudo sysctl -p
-sudo systemctl restart procps
 
 echo -e "${NC}Script Complete"
