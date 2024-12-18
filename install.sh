e5c7b6072dec603c2f3fc28ccdc5299f4da1aff8
BennyDaBee <97147515+BennyDaBee@users.noreply.github.com>
2024-12-18 01:58:42 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,10 +25,17 @@ sudo apt-get upgrade -y
 echo -e "${GREEN}Complete"
 
 echo -e "${BLUE}Installing Wazuh"
-sudo wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.3-1_amd64.deb && sudo WAZUH_MANAGER='172.24.10.24' dpkg -i ./wazuh-agent_4.7.3-1_amd64.deb
+sudo wget --inet4-only https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.3-1_amd64.deb && sudo WAZUH_MANAGER='172.24.10.24' dpkg -i ./wazuh-agent_4.7.3-1_amd64.deb
 sudo systemctl daemon-reload
 sudo systemctl enable wazuh-agent
 sudo systemctl start wazuh-agent
+echo -e "${GREEN}Complete"
+
+echo -e "${CYAN}Configuring Netplan"
+sudo wget --inet4-only https://raw.githubusercontent.com/BennyDaBee/Ubunut-Create/main/00-installer-config.yaml -O /tmp/00-installer-config.yml
+sudo mv /etc/netplan/00-installer-config.yml /etc/netplan/00-installer-config.yml.old
+sudo mv /tmp/00-installer-config.yml /etc/netplan/00-installer-config.yml
+sudo netplan apply
 echo -e "${GREEN}Complete"
 
 if grep -Fxq "HostKeyAlgorithms +ssh-rsa" /etc/ssh/sshd_config; then
